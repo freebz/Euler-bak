389e773f7405a00313a3f3cfdc645b191cda8b80
FreeBz <freebz.dev@gmail.com>
2014-04-25 01:34:23 +0900

    2번문제 C로 작성

---
+++ p002/c/p002.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+long fibo(long n);
+
+int main() {
+  int i = 1;
+  long result = 0, f = 0;
+  while (f < 4000000) {
+    f = fibo(i++);
+    if (f % 2 == 0) {
+      result += f;
+    }
+  }
+
+  printf("%d", result);
+  return 0;
+}
+
+long fibo(long n) {
+  if (n == 1) return 1;
+  if (n == 2) return 2;
+  return fibo(n-1) + fibo(n-2);
+}
