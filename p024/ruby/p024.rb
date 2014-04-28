53e97e84a96f2a17195815229b609c9023f7060b
FreeBz <freebz.dev@gmail.com>
2014-04-28 10:43:20 +0900

    24번 문제 Ruby로 작성

--- p024/ruby/p024.rb
+++ p024/ruby/p024.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 
-SIZE = 9
+LAST = 9
 LIMIT = 1_000_000
 
-p (0..SIZE).to_a.permutation.lazy.drop(LIMIT-1).take(1).force.join
+p (0..LAST).to_a.permutation.lazy.drop(LIMIT-1).take(1).force.join
