5e1f9f0ff1174ad66f1e65460a0ef90cdb98b770
FreeBz <freebz.dev@gmail.com>
2014-04-30 20:52:02 +0900

    전부 1로 초기화 하는 대신 1의 경우도 연산

--- p031/c/p031.c
+++ p031/c/p031.c
@@ -5,13 +5,9 @@
 int main(){
 
   int i, j;
-  int data[LIMIT+1];
-  int coin[] = {2, 5, 10, 20, 50, 100, 200};
+  int data[LIMIT+1] = {1};
+  int coin[] = {1, 2, 5, 10, 20, 50, 100, 200};
   int coinLen = sizeof(coin) / sizeof(coin[0]);
-
-  for (i = 0; i <= LIMIT; i++) {
-    data[i] = 1;
-  }
 
   for (i = 0; i < coinLen; i++) {
     for (j = coin[i]; j <= LIMIT; j++) {
