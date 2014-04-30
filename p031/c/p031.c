0eabd41de766d0f5ad5405a867afd9cb7edd4993
FreeBz <freebz.dev@gmail.com>
2014-04-30 17:20:43 +0900

    31번 문제 C로 작성

---
+++ p031/c/p031.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define LIMIT 200
+
+int main(){
+
+  int i, j;
+  int data[LIMIT+1];
+  int coin[] = {2, 5, 10, 20, 50, 100, 200};
+  int coinLen = sizeof(coin) / sizeof(coin[0]);
+
+  for (i = 0; i <= LIMIT; i++) {
+    data[i] = 1;
+  }
+
+  for (i = 0; i < coinLen; i++) {
+    for (j = coin[i]; j <= LIMIT; j++) {
+      data[j] += data[j-coin[i]];
+    }
+  } 
+
+  printf("%d\n", data[LIMIT]);
+  return 0;
+}
